df798af3e74121dc2a929af2091d6383bab7c459
mivuyolm <2562730@students.wits.ac.za>
2025-04-09 19:52:08 +0200

    created search homepage (#2)
    Co-authored-by: mivuyolm <2562730@student.wits.ac.za>

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ConstitutionalArchiveHomepage from './ConstitutionalArchiveHomepage';
 
 
 function App() {
   return (
     <div className="App">
-     <p>Hello Patzers!</p>
+     <ConstitutionalArchiveHomepage/>
     </div>
   );
 }
