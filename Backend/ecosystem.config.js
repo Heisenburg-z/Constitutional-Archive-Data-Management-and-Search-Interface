276c0dd0704dc2e6796e4882ac211cd292aa181e
Heisenburg-z <2603646@students.wits.ac.za>
2025-04-13 02:50:35 +0200

    fix: server update

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    apps: [{
-      name: "constitutional-archive-api",
-      script: "./index.js",
-      instances: "max",
-      exec_mode: "cluster",
-      env: {
-        NODE_ENV: "production"
-      }
-    }]
-  }
+  apps: [{
+    name: "constitutional-archive-api",
+    script: "./index.js",
+    instances: 1, // Run only 1 instance
+    exec_mode: "fork", // Use fork mode
+    env: {
+      NODE_ENV: "production",
+    }
+  }]
+};
