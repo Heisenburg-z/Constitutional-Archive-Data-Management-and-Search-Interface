b033c9a7e079e2a9b61319ea36b83c2ea6e8d1f9
Heisenburg-z <2603646@students.wits.ac.za>
2025-04-14 08:27:36 +0200

    removed tests tempo

--- Frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hello message', () => {
-  render(<App />);
-  const messageElement = screen.getByText(/hello patzers!/i);
-  expect(messageElement).toBeInTheDocument();
-});
